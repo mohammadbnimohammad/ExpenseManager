1c4f770e491f0756ba2e37a513c02fff8af1e6ee
MohammadBaniMohammad <mohjavadev91@gmail.com>
2025-05-26 22:38:55 +0300

    creat dto layer userDto

---
+++ src/main/java/com/mohammad/ExpenseManager/dto/UserDto.java
@@ -0,0 +1,22 @@
+package com.mohammad.ExpenseManager.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private  String username;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email is must be valid ")
+    private  String email;
+    @NotBlank(message = "password is required")
+    @Size(min = 8, max = 15, message = "Password must be between 8 and 15 characters")
+    private  String password;
+
+
+}
