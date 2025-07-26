ade05fe2249886733d02b9e48072c973477f2bbd
MohammadBaniMohammad <mohjavadev91@gmail.com>
2025-07-26 10:34:11 +0300

    add  income Entity

---
+++ src/main/java/com/mohammad/ExpenseManager/model/Income.java
@@ -0,0 +1,45 @@
+package com.mohammad.ExpenseManager.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="income")
+public class Income {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "The title is required ")
+    @Size(min = 3,max = 20,message = "the title should between 3-20 characters")
+    @Column(nullable = false)
+    private String title;
+
+
+    @NotNull(message = "The amount is required")
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false,name = "income_date")
+    private LocalDate date;
+
+    @Size(max = 200 )
+    private String description;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
