51dfefca4bed03f692dc4e1f26dbe9fef959fbb0
MohammadBaniMohammad <mohjavadev91@gmail.com>
2025-08-06 20:08:21 +0300

    remove space

--- src/main/java/com/mohammad/ExpenseManager/service/IncomeService.java
+++ src/main/java/com/mohammad/ExpenseManager/service/IncomeService.java
@@ -2,10 +2,6 @@ package com.mohammad.ExpenseManager.service;
 
 import com.mohammad.ExpenseManager.dto.IncomeDto;
 import com.mohammad.ExpenseManager.dto.IncomeResponseDto;
-import com.mohammad.ExpenseManager.model.Income;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface IncomeService  {
     public IncomeResponseDto createIncome (IncomeDto incomeDto);
