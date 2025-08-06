55c0c74e8f682f9ac6cabeaf0238e1b1b419c10c
MohammadBaniMohammad <mohjavadev91@gmail.com>
2025-08-06 09:09:47 +0300

    create income service interface and there methodes

---
+++ src/main/java/com/mohammad/ExpenseManager/service/IncomeService.java
@@ -0,0 +1,14 @@
+package com.mohammad.ExpenseManager.service;
+
+import com.mohammad.ExpenseManager.dto.IncomeDto;
+import com.mohammad.ExpenseManager.dto.IncomeResponseDto;
+
+import java.util.List;
+
+public interface IncomeService {
+    public IncomeResponseDto createIncome (IncomeDto incomeDto);
+    public List<IncomeResponseDto> getAllIncomeForCurrentUser();
+    public IncomeResponseDto getIncomeById(Long id);
+    public IncomeResponseDto updateIncome(Long id,IncomeDto incomeDto);
+    public void deleteIncome(Long id);
+}
